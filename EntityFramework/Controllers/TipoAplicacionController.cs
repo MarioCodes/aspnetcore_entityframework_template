377ce071c8fb4e954287837e01e28179f25283e8
Mario Codes <mariocodes1993@gmail.com>
2024-02-26 16:30:48 +0100

    added same changes to TipoAplicacion

--- EntityFramework/Controllers/TipoAplicacionController.cs
+++ EntityFramework/Controllers/TipoAplicacionController.cs
@@ -15,19 +15,73 @@ namespace EntityFramework.Controllers
                 _context = context;
         }
 
-        [HttpPost]
-        public IActionResult Crear(TipoAplicacion tipoAplicacion)
-        {
-            _context.TipoAplicacion.Add(tipoAplicacion);
-            _context.SaveChanges();
-            return Ok("created category");
-        }
-
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult GetAll()
         {
             IEnumerable<TipoAplicacion> tipos = _context.TipoAplicacion;
             return Ok(tipos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOne(int id)
+        {
+            if(id == 0)
+            {
+                return NotFound();
+            }
+
+            TipoAplicacion? obj = _context.TipoAplicacion.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(obj);
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(TipoAplicacion tipoAplicacion)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.TipoAplicacion.Add(tipoAplicacion);
+                _context.SaveChanges();
+                return Ok("created category");
+            }
+
+            return StatusCode(500, "cannot create category");
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(TipoAplicacion tipoAplicacion)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.TipoAplicacion.Update(tipoAplicacion);
+                _context.SaveChanges();
+                return Ok("updated category");
+            }
+
+            return StatusCode(500, "cannot update tipoAplication");
+        }
+
+        [HttpGet("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            if(id == 0)
+            {
+                return NotFound();
+            }
+
+            TipoAplicacion? obj = _context.TipoAplicacion.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _context.TipoAplicacion.Remove(obj);
+            _context.SaveChanges();
+            return Ok("tipoAplicacion deleted");
         }
     }
 }
